03122dec258fb12f8410d1d7f7c0c8ce933547dc
Ohad Shapira <Ohad@Ohads-MacBook-Pro.local>
2020-01-29 17:01:17 +0200

    apt. seller complete

--- apt-filter.js
+++ apt-filter.js
@@ -1,8 +1,10 @@
-//Only start playing with this file if you're done with all the basics
+
 
 const must = (radio) => radio.filter(rb => rb.checked).length
 
-const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
+
+
+const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate, parking) {
     let relevantApts = apartments.filter(a =>
         a.address.toLowerCase().includes(address.toLowerCase() || "") &&
         a.price >= (minPrice || 0) &&
@@ -12,5 +14,7 @@ const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRoo
     )
 
     immediate = !immediate[0].checked
-    return immediate ? relevantApts : relevantApts.filter(a => (a.immediate)) 
+    return immediate ? 
+    (!parking[0].checked ? relevantApts : relevantApts.filter(a => a.parking)) :
+    (!parking[0].checked ? relevantApts.filter(a => (a.immediate)): relevantApts.filter(a => (a.immediate)).filter(a => a.parking) )
 }
--- main.js
+++ main.js
@@ -6,15 +6,26 @@ $("button").on("click", function () {
     let minRooms = $("#min-r-input").val()
     let maxRooms = $("#max-r-input").val()
     let immediate = $("#immed-y")
+    let parking = $("#park-y")
 
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
-    renderApts(relevantApts)
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate, parking)
+   
+    if(relevantApts.length < 1 ) {
+        $("#results").empty()   
+        $("#results")
+        .append("<div class=no-match>You're waaayy too picky you spoiled JAP </div>")
+    } else{
+        renderApts(relevantApts)
+    }
+    
 })
 
 const renderApts = function (apartments) {
     $("#results").empty()
-    console.log(apartments) //array of apartments to render
-    //Your code goes here.
+    const source = $('#immed-template').html();
+    const template = Handlebars.compile(source);
+    const newHTML = template({apts: apartments});
+    $('#results').append(newHTML);
 }
 
-renderApts(apartments) //renders apartments when page loads
+renderApts(apartments) 
